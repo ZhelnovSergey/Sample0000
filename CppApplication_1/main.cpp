e05572248b4f2c0f3d2e45aa6b7f1f0fac85f75b
sergey <sergey@zhelnov>
2018-04-11 12:20:36 +0300

    123

---
+++ CppApplication_1/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: sergey
+ *
+ * Created on April 11, 2018, 12:11 PM
+ */
+
+#include <stdio.h>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("Hello GIT!");
+    return 0;
+}
+
